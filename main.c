e52f9450df2bb748fea2227cdb5830c27a14c62b
Kevin Chang <kevin@kevinwchang.com>
2015-04-16 14:56:10 -0700

    ok at 255 with 3 weights

--- main.c
+++ main.c
@@ -247,7 +247,7 @@ int main()
 	long integral = 0;
   
   int max = 0;
-  int const MaxEver = 240;
+  int const MaxEver = 255;
   unsigned int startMs;
 
 	init_motors();
@@ -338,7 +338,7 @@ int main()
 			// turn to the left, and the magnitude of the number determines
 			// the sharpness of the turn.
 			//int power_difference = proportional/4 + derivative*18;
-			int power_difference = proportional*2/5 + derivative*20;
+			int power_difference = proportional/2 + derivative*20;
 
 			// Compute the actual motor settings.  We never set either motor
 			// to a negative value.
